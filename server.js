12fdbaad7685f701d75435f8c186669c8c009c5e
Josh Carter <joshcarter8400@gmail.com>
2020-11-01 15:22:02 -0500

    added homepage template

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const routes = require("./routes");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
-
+const path = require("path");
 const app = express();
+const hbs = exphbs.create({});
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // turn on routes
 app.use(routes);

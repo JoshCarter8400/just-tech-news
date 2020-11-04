aede9b14cca1196ec86f689f9f0a28c245f7e641
Josh Carter <joshcarter8400@gmail.com>
2020-11-04 13:10:02 -0500

    added helper functions and test

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 const path = require("path");
 const app = express();
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());

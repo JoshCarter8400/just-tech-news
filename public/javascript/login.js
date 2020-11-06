15d11f30ac60645e936dd51e4659d3933991a0a3
Josh Carter <joshcarter8400@gmail.com>
2020-11-05 21:32:44 -0500

    fixing login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -19,6 +19,7 @@ async function signupFormHandler(event) {
     // check the response status
     if (response.ok) {
       console.log("success");
+      document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }
@@ -33,7 +34,7 @@ async function loginFormHandler(event) {
 
   if (email && password) {
     const response = await fetch("/api/users/login", {
-      method: "post",
+      method: "POST",
       body: JSON.stringify({
         email,
         password,
